25beebdbe658936a7bbada628b64c24623a38252
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-02-01 01:15:43 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <div>
         <h1>Hello, it&apos;s homepage here!</h1>
+        <ul>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/clients">Clients</Link>
+          </li>
+          <li>
+            <Link href="about">About</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
