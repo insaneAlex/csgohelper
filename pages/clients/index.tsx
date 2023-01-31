d8384cd92b8fa72eea03064b43468838cde1d719
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-02-01 01:26:35 +0200

    update

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "ped", name: "Pedro" },
+    { id: "alex", name: "Oleksandr" },
+  ];
   return (
     <div>
       <h1>Clients Page</h1>
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
