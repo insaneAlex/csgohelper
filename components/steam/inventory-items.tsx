4aa2e90bf791d896d9555fba41d41ea90dafcdf8
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-02-21 01:15:13 +0200

    update

--- components/steam/inventory-items.tsx
+++ components/steam/inventory-items.tsx
@@ -1,21 +1,32 @@
-import {FC} from "react";
+import {FC, useRef} from "react";
 import styles from "./inventory-items.module.scss";
 import {InventoryItem} from "./inventory-item";
 import {SortedInventoryItemType, SortedInventoryType} from "./types";
 
-type Props = {items: SortedInventoryType; onSearch: () => void};
+type Props = {
+  items: SortedInventoryType;
+  onSearch: ({steamId}: {steamId?: string}) => void;
+};
 
 export const InventoryList: FC<Props> = ({items, onSearch}) => {
   const {total_inventory_count: inventoryAmount, inventory} = items;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    console.log(inputRef);
+    onSearch({steamId: inputRef?.current?.value});
+  };
 
   return (
     <>
       <h2 className={styles.title}>{`Total Items: ${inventoryAmount}`}</h2>
       <div className={styles.searchBlock}>
-        <form action="">
+        <form autoComplete="true" action="">
           <div>
             <label htmlFor="steamId"></label>
             <input
+              ref={inputRef}
+              className={styles.input}
               placeholder="Enter your SteamID64"
               type="text"
               id="steamId"
@@ -23,15 +34,17 @@ export const InventoryList: FC<Props> = ({items, onSearch}) => {
           </div>
         </form>
         <div className={styles.searchBtn}>
-          <button onClick={onSearch}>SEARCH BY SteamID</button>
+          <button onClick={handleClick}>SEARCH BY SteamID</button>
         </div>
       </div>
 
-      <ul className={styles.items}>
-        {inventory.map((item: SortedInventoryItemType, index) => (
-          <InventoryItem key={index} item={item} />
-        ))}
-      </ul>
+      <div className={styles.container}>
+        <ul className={styles.items}>
+          {inventory.map((item: SortedInventoryItemType, index) => (
+            <InventoryItem key={index} item={item} />
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,6 +1,6 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({message: "Hello man"});
 };
 
