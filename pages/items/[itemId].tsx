a3f189c9968e68472e8507290a02912b542b6530
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-09-25 22:42:17 +0300

    chg: dev: Update

--- pages/items/[itemId].tsx
+++ pages/items/[itemId].tsx
@@ -6,26 +6,30 @@ import {ItemDetails} from "@/components/steam";
 import {DUMMY_INVENTORY} from "@/dummy/data";
 import {useRouter} from "next/router";
 
-type Props = {inventory: InventoryItemType[]};
-
-const ItemDetailsPage: FC<Props> = ({inventory = DUMMY_INVENTORY}) => {
+const ItemDetailsPage: FC = () => {
+  const items = DUMMY_INVENTORY;
   const router = useRouter();
-  const itemId = router.query.itemId;
+  const itemId = router?.query?.itemId;
 
-  const item = inventory?.find((item) => item?.assetid === itemId);
-
-  if (!item) {
+  if (!items) {
     return <Loader />;
   }
-  return (
-    <>
-      <Link href="/">
-        <BackIcon height={32} width={32} />
-      </Link>
 
-      <ItemDetails item={item as InventoryItemType} />
-    </>
-  );
+  const item = items?.find((item) => item?.assetid === itemId);
+
+  if (item) {
+    return (
+      <>
+        <Link href="/">
+          <BackIcon height={32} width={32} />
+        </Link>
+
+        <ItemDetails item={item as unknown as InventoryItemType} />
+      </>
+    );
+  }
+
+  return <></>;
 };
 
 export default ItemDetailsPage;
