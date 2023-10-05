0d5cc003cb0bfda2456ad9164f4dca5f47f1853b
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-06 00:05:56 +0300

    chg: dev: Update

--- api/constants.ts
+++ api/constants.ts
@@ -1,5 +1,4 @@
-export const inventoryImageBaseUrl =
-  "https://steamcommunity-a.akamaihd.net/economy/image/";
+export const inventoryImageBaseUrl = 'https://steamcommunity-a.akamaihd.net/economy/image/';
 
-export const getInventoryEndpoint = "http://localhost:5000/v1/csgoInventory";
-export const getItemPriceEndpoint = "http://localhost:5000/v1/itemPrice";
+export const getInventoryEndpoint = 'http://localhost:5000/v1/csgoInventory';
+export const getItemPriceEndpoint = 'http://localhost:5000/v1/itemPrice';
--- api/get-item-price.ts
+++ api/get-item-price.ts
@@ -1,15 +1,12 @@
-import {getItemPriceEndpoint} from "./constants";
+import {getItemPriceEndpoint} from './constants';
 
 export const getItemPrice = async ({hashName}: {hashName?: string}) => {
   const getPriceUrl = `${getItemPriceEndpoint}?hashName=${hashName}`;
 
   try {
-    const response = await fetch(getPriceUrl, {
-      method: "GET",
-      headers: {"Content-Type": "application/json"},
-    });
+    const response = await fetch(getPriceUrl);
     return response.json();
   } catch (e) {
-    console.log("Something horrible happened");
+    console.log('Something horrible happened');
   }
 };
