fb17207f78d9a1d44f5cc0f5d6d944cd55c26f1a
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-14 00:01:24 +0300

    chg: dev: Minor change

--- src/steam/components/item-details/item-details.tsx
+++ src/steam/components/item-details/item-details.tsx
@@ -10,14 +10,11 @@ type Props = {item: InventoryItemType};
 export const ItemDetails: FC<Props> = ({item}) => {
   const {icon_url, name, exterior} = item;
 
-  const imgSrc = `${inventoryImageBaseUrl}${icon_url}`;
-
   return (
     <div className={styles.details}>
       <h1>{name}</h1>
       {exterior && <h2 className={styles.exterior}>{`(${exterior})`}</h2>}
-      <Image src={imgSrc} alt={name} quality={100} width={256} priority height={198} />
-      <hr className={styles.line} />
+      <Image src={inventoryImageBaseUrl + icon_url} alt={name} quality={100} width={256} priority height={198} />
     </div>
   );
 };
