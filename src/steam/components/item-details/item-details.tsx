fd3762d4baa6cdb700ed23eedc607d57f962f214
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-13 23:59:14 +0300

    chg: dev: Fix lint error

--- src/steam/components/item-details/item-details.tsx
+++ src/steam/components/item-details/item-details.tsx
@@ -8,32 +8,16 @@ import styles from './item-details.module.scss';
 type Props = {item: InventoryItemType};
 
 export const ItemDetails: FC<Props> = ({item}) => {
-  const {icon_url, name, descriptions, exterior} = item;
+  const {icon_url, name, exterior} = item;
 
   const imgSrc = `${inventoryImageBaseUrl}${icon_url}`;
 
-  const renderDescriptions = () => (
-    <div className={styles.descriptions}>
-      {descriptions &&
-        descriptions.map(({value, color}, i) => {
-          return (
-            value && (
-              <div key={i} style={color ? {color: `#${color}`} : undefined}>
-                {value}
-              </div>
-            )
-          );
-        })}
-    </div>
-  );
   return (
     <div className={styles.details}>
       <h1>{name}</h1>
       {exterior && <h2 className={styles.exterior}>{`(${exterior})`}</h2>}
       <Image src={imgSrc} alt={name} quality={100} width={256} priority height={198} />
       <hr className={styles.line} />
-
-      {renderDescriptions()}
     </div>
   );
 };
