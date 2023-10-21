0b1d7c0228a043aa5efa9a9cfca2814d88a0ce70
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-21 07:42:11 +0300

    chg: dev: Fix calculateInventoryPrice

--- src/steam/helpers/calculate-inventory-price.ts
+++ src/steam/helpers/calculate-inventory-price.ts
@@ -1,11 +1,11 @@
 import {PriceOptions} from '@/api/types';
 import {InventoryItemType} from '@/types';
 
-export const calculateInventoryPrice = ({items}: {items: InventoryItemType[]}) =>
-  items
-    .reduce((accumulator, currentValue) => {
-      const price = Number(currentValue.prices?.[PriceOptions.WEEK]?.median);
-      const count = currentValue?.count || 1;
-      return isNaN(price) ? accumulator : accumulator + price * count;
-    }, 0)
-    .toFixed(2);
+export const calculateInventoryPrice = ({items}: {items: InventoryItemType[]}) => {
+  const total = items.reduce((accumulator, currentValue) => {
+    const price = Number(currentValue.prices?.[PriceOptions.WEEK]?.median);
+    const count = currentValue?.count || 1;
+    return isNaN(price) ? accumulator : accumulator + price * count;
+  }, 0);
+  return total === 0 ? '' : total.toFixed(2);
+};
