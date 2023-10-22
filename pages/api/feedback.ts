e727717a07b4b326918cfc45296938b5c6ca6015
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-23 01:59:12 +0300

    fix: dev: Endpoint refactor

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,16 +1,7 @@
-import axios from 'axios';
 import {NextApiRequest, NextApiResponse} from 'next';
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const {data} = await axios.get('https://csgobackpack.net/');
-    console.log(data);
-
-    res.status(200).json({response: data});
-  } catch (error) {
-    res.status(200).json({error});
-    console.log(`error on fetching https://csgobackpack.net/: ${error}`);
-  }
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({message: 'Hello man'});
 };
 
 export default handler;
