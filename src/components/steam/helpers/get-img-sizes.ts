7d8e9eb41c4f1e678dc56bc101c12f52d350f992
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-24 05:04:50 +0300

    minor refactor

--- src/components/steam/helpers/get-img-sizes.ts
+++ src/components/steam/helpers/get-img-sizes.ts
@@ -2,25 +2,14 @@ import {ImgSize} from '@/types';
 import {ScreenSizes} from '../constants';
 
 export const getImgSizes = ({screenSize}: {screenSize: ScreenSizes}): ImgSize => {
-  const imgSizes = {
-    lg: {width: 128, height: 99},
-    md: {width: 110, height: 82},
-    sm: {width: 140, height: 108},
-    xs: {width: 77, height: 60}
-  };
+  const imgSizes = {md: {width: 110, height: 82}, sm: {width: 140, height: 108}, xs: {width: 77, height: 60}};
 
   switch (screenSize) {
-    case ScreenSizes.Lg:
-      return imgSizes.md;
-    case ScreenSizes.Md:
-      return imgSizes.md;
-    case ScreenSizes.Sm:
-      return imgSizes.md;
     case ScreenSizes.Xs:
       return imgSizes.xs;
     case ScreenSizes.Xxs:
       return imgSizes.sm;
     default:
-      return imgSizes.lg;
+      return imgSizes.md;
   }
 };
