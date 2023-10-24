6f60b0f6e05134b74939d596d3f20a8a48ea170f
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-24 04:51:11 +0300

    fix: usr: Updated view

--- src/components/steam/helpers/get-img-sizes.ts
+++ src/components/steam/helpers/get-img-sizes.ts
@@ -2,7 +2,12 @@ import {ImgSize} from '@/types';
 import {ScreenSizes} from '../constants';
 
 export const getImgSizes = ({screenSize}: {screenSize: ScreenSizes}): ImgSize => {
-  const imgSizes = {lg: {width: 128, height: 99}, md: {width: 110, height: 82}, sm: {width: 55, height: 41}};
+  const imgSizes = {
+    lg: {width: 128, height: 99},
+    md: {width: 110, height: 82},
+    sm: {width: 140, height: 108}
+  };
+
   switch (screenSize) {
     case ScreenSizes.Lg:
       return imgSizes.md;
@@ -11,7 +16,7 @@ export const getImgSizes = ({screenSize}: {screenSize: ScreenSizes}): ImgSize =>
     case ScreenSizes.Sm:
       return imgSizes.md;
     case ScreenSizes.Xs:
-      return imgSizes.sm;
+      return imgSizes.md;
     case ScreenSizes.Xxs:
       return imgSizes.sm;
     default:
