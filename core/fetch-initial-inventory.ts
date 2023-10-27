9825cdae3a00054701c8204511a2882fe8c78a49
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-10-27 13:20:08 +0300

    linter fix

--- core/fetch-initial-inventory.ts
+++ core/fetch-initial-inventory.ts
@@ -3,12 +3,14 @@ import {InitialInventoryResponseType} from './types';
 import {storage} from '@/src/services';
 
 type fetchInitialInventoryType = (a: Props) => Promise<InitialInventoryResponseType>;
-
 type Props = {signal: AbortSignal};
+
 export const fetchInitialInventory: fetchInitialInventoryType = async ({signal}) => {
   const storedSteamid = storage.localStorage.get(STEAMID_PARAM);
-  const url = storedSteamid && `${fetchInventoryUrl}?storedSteamid=${storedSteamid}`;
+  if (storedSteamid) {
+    const url = storedSteamid && `${fetchInventoryUrl}?storedSteamid=${storedSteamid}`;
 
-  const response = await fetch(url, {signal});
-  return response.json();
+    const response = await fetch(url, {signal});
+    return response.json();
+  }
 };
