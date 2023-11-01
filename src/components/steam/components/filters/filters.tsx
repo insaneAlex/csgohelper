73b5a5bda1c45c83291e1a1626ffc5d81e687fcf
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-01 22:39:01 +0200

    chg: dev: Filters refactor

--- src/components/steam/components/filters/filters.tsx
+++ src/components/steam/components/filters/filters.tsx
@@ -5,23 +5,18 @@ import {useSelector} from 'react-redux';
 import {isFilterApplied} from './helpers';
 import styles from './filters.module.scss';
 import {itemsFiltersSelector} from '@/src/redux';
+import {getParamValue} from '../../helpers';
 
 export const Filters: FC = () => {
   const router = useRouter();
   const possibleFilters = useSelector(itemsFiltersSelector);
 
   const handleFilterUpdate = (filterName: string, value: string) => {
-    const currentValue = router.query[filterName];
-    const isApplied = isFilterApplied(currentValue, value);
+    const currentValue = getParamValue(router, filterName);
+    const filterIsApplied = isFilterApplied(currentValue, value);
 
-    let newFilterValue;
-    if (Array.isArray(currentValue)) {
-      newFilterValue = isApplied ? currentValue.filter((v) => v !== value) : [...currentValue, value];
-    } else if (typeof currentValue === 'string') {
-      newFilterValue = isApplied ? [] : [currentValue, value];
-    } else {
-      newFilterValue = [value];
-    }
+    const newFilterValue = filterIsApplied ? currentValue.filter((v) => v !== value) : [...currentValue, value];
+
     return router.push({query: {...router.query, [filterName]: newFilterValue}});
   };
 
