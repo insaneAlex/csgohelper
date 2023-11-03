05ac626deec70bebd5b67e7e8a45537fb8bb911c
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-03 13:36:44 +0200

    chg: dev: getAppliedFilterParams refactor

--- src/components/steam/helpers/get-applied-filter-params.ts
+++ src/components/steam/helpers/get-applied-filter-params.ts
@@ -5,28 +5,18 @@ export const getAppliedFilterParams = (possibleFilters: {[key: string]: string[]
   const filters: {[key: string]: string[]} = {};
   const parameters = {...params};
 
-  const possibleFilterTypes = Object.keys(possibleFilters);
-
-  const filterTypes: string[] = [];
-
   const typeParams = getParamValuesArray(parameters, 'type');
-  typeParams.forEach((type) => possibleFilterTypes.includes(type) && filterTypes.push(type));
-
-  if (filterTypes.length > 0) {
-    filters.types = filterTypes;
+  if (typeParams.length > 0) {
+    const filterTypes = typeParams.filter((type) => possibleFilters[type]);
+    if (filterTypes.length > 0) {
+      filters.types = filterTypes;
+    }
   }
-
-  parameters.type && delete parameters.type;
+  delete parameters.type;
 
   Object.keys(parameters).forEach((param) => {
     if (param in possibleFilters) {
-      filters[param] = [];
-      const paramValues = getParamValuesArray(parameters, param);
-      paramValues.forEach((val) => {
-        if (possibleFilters[param].includes(val)) {
-          filters[param].push(val);
-        }
-      });
+      filters[param] = getParamValuesArray(parameters, param).filter((val) => possibleFilters[param].includes(val));
     }
   });
 
