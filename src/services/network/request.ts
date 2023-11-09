b63c3d8a344dc85cb9ce955a7668933032cd6f41
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-09 20:33:24 +0200

    chg: dev: update

--- src/services/network/request.ts
+++ src/services/network/request.ts
@@ -1,12 +1,20 @@
 import {bareRequest} from './network';
 import {FetchOptions, RequestBody} from './types';
 
-const get = <T>(url: string, options: FetchOptions = {}): Promise<T> => bareRequest(url, options);
+const get = <T>(url: string, options: FetchOptions = {}): Promise<T> => {
+  const getOptions = {
+    headers: {'Content-Type': 'application/json'},
+    ...options
+  };
+
+  return bareRequest(url, getOptions);
+};
 
 const post = <T>(url: string, body: RequestBody, options?: FetchOptions): Promise<T> => {
   const postOptions = {
     ...options,
     method: 'POST',
+    headers: {'Content-Type': 'application/json'},
     body: JSON.stringify(body)
   };
   return bareRequest(url, postOptions);
