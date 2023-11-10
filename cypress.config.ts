c80b4579bd880af90a9ccdf4804aae99c4b90d71
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-10 18:48:55 +0200

    chg: dev:  update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from 'cypress';
 
 export default defineConfig({
   projectId: 'nk6kgn',
+
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
+      framework: 'next',
+      bundler: 'webpack'
+    }
   },
+
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(_on, _config) {
+      // implement node event listeners here
+    }
+  }
 });
