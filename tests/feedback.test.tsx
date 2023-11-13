6bacf91e2fe1d37590af2e75ed98b49a099d7f42
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-13 17:49:30 +0200

    chg: dev: update

--- tests/feedback.test.tsx
+++ tests/feedback.test.tsx
@@ -1,13 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {FeedbackStatuses, onResetState} from '../src/redux/features';
 import {render, screen} from '@testing-library/react';
 import Feedback from '../pages/feedback';
+import {useSelector} from 'react-redux';
 
+const dispatchMock = jest.fn();
 jest.mock('../src/services', () => ({noop: () => jest.fn()}));
 jest.mock('next/router', () => ({useRouter: () => ({push: jest.fn()})}));
-jest.mock('react-redux', () => ({useDispatch: () => jest.fn(), useSelector: jest.fn()}));
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => dispatchMock
+}));
 
 describe('Feedback', () => {
-  it('should render correctly', () => {
-    render(<Feedback />);
-    expect(screen.getByTestId('feedback-form')).toBeInTheDocument();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  describe('on initial load', () => {
+    it('should render form', () => {
+      (useSelector as any).mockReturnValueOnce(FeedbackStatuses.IDLE);
+      render(<Feedback />);
+      expect(screen.getByTestId('feedback-form')).toBeInTheDocument();
+    });
+  });
+  describe('when feedback is success', () => {
+    it('should render return to home btn', () => {
+      (useSelector as any).mockReturnValueOnce(FeedbackStatuses.SUCCESS);
+      render(<Feedback />);
+      expect(screen.getByText('Return to Home')).toBeInTheDocument();
+    });
+  });
+  describe('when unmounting', () => {
+    it('should call onResetState once', () => {
+      const utils = render(<Feedback />);
+      utils.unmount();
+      expect(dispatchMock).toHaveBeenCalledTimes(1);
+      expect(dispatchMock).toHaveBeenCalledWith(onResetState());
+    });
   });
 });
