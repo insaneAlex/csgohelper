4d7fe30dda679a500215a6bb3f1852d89182d4b4
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-16 20:42:02 +0200

    chg: dev: improved price accessability

--- src/components/steam/components/inventory-item/inventory-item.tsx
+++ src/components/steam/components/inventory-item/inventory-item.tsx
@@ -13,11 +13,19 @@ import styles from './inventory-item.module.scss';
 
 type Props = {imgSize: ImgSize; item: InventoryItemType & Layout};
 
+// eslint-disable-next-line complexity
 export const InventoryItem: FC<Props> = ({item, imgSize}) => {
   const router = useRouter();
   const {name_color, assetid, icon_url, count = 1, prices} = item;
   const {width, height} = imgSize;
-  const price = prices?.[PriceOptions.WEEK]?.average * count;
+
+  const availablePrice =
+    prices?.[PriceOptions.DAY] ||
+    prices?.[PriceOptions.WEEK] ||
+    prices?.[PriceOptions.MONTH] ||
+    prices?.[PriceOptions.ALL];
+
+  const price = availablePrice?.average * count;
   const formattedPrice = !isNaN(price) && price.toFixed(2);
 
   if (!icon_url) {
--- src/components/steam/helpers/calculate-inventory-price.ts
+++ src/components/steam/helpers/calculate-inventory-price.ts
@@ -2,10 +2,16 @@ import {InventoryItemType} from '@/src/services/steam-inventory';
 import {PriceOptions} from '../types';
 
 export const calculateInventoryPrice = ({items}: {items: InventoryItemType[]}) => {
-  const total = items.reduce((accumulator, currentValue) => {
-    const price = Number(currentValue?.prices?.[PriceOptions.WEEK]?.median);
-    const count = currentValue?.count || 1;
-    return isNaN(price) ? accumulator : accumulator + price * count;
+  const total = items.reduce((accumulator, {prices, count}) => {
+    const availablePrice =
+      prices?.[PriceOptions.DAY] ||
+      prices?.[PriceOptions.WEEK] ||
+      prices?.[PriceOptions.MONTH] ||
+      prices?.[PriceOptions.ALL];
+
+    const price = Number(availablePrice?.average);
+    const amount = count || 1;
+    return isNaN(price) ? accumulator : accumulator + price * amount;
   }, 0);
   return total === 0 ? '' : total.toFixed(2);
 };
