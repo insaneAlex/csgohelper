292419ad95324276f1d6805ec11e17318c438194
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2023-11-18 16:57:58 +0200

    chg: dev: added provider test

---
+++ src/redux/tests/provider.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {ReduxProvider} from '../provider';
+import {store} from '../store';
+import {ReactReduxContext} from 'react-redux';
+
+jest.mock('../../services', () => ({}));
+describe('ReduxProvider Component', () => {
+  it('renders children with Redux store', () => {
+    render(
+      <ReduxProvider>
+        <div className="test-child">Test Child</div>
+      </ReduxProvider>
+    );
+    expect(screen.getByText('Test Child')).toBeInTheDocument();
+  });
+  it('passes Redux store to children', () => {
+    let passedStore = null;
+    const TestComponent = () => {
+      passedStore = React.useContext(ReactReduxContext).store;
+      return null;
+    };
+    render(
+      <ReduxProvider>
+        <TestComponent />
+      </ReduxProvider>
+    );
+    expect(passedStore).toBe(store);
+  });
+});
