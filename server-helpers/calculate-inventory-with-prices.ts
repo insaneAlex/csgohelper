7c8fc48bd57549f7a5c7574bcf8e3b9cd1f9908c
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2024-01-12 14:57:24 +0200

    Merge branch 'main' of https://github.com/insaneAlex/nextapp

--- server-helpers/calculate-inventory-with-prices.ts
+++ server-helpers/calculate-inventory-with-prices.ts
@@ -1,9 +1,5 @@
 import {NoPriceInventory} from '@/pages/api/csgoInventory';
-import {InventoryItemType} from '@/src/services';
 import {PricesType} from '@/src/services/aws/types';
 
-export const calculateInventoryWithPrices = (inventory: NoPriceInventory, prices: PricesType) => {
-  return (
-    prices ? inventory.map((item) => ({...item, prices: prices?.[item.market_hash_name]?.price})) : inventory
-  ) as InventoryItemType[];
-};
+export const calculateInventoryWithPrices = (inventory: NoPriceInventory, prices: PricesType) =>
+  prices ? inventory.map((item) => ({...item, prices: prices?.[item.market_hash_name]?.price})) : inventory;
