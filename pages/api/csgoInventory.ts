a8b6be9f86eb7207ff8f91f37029181a972cb8ee
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2024-03-18 05:43:11 +0200

    new: usr: Search by profile link and custom url added

--- pages/api/csgoInventory.ts
+++ pages/api/csgoInventory.ts
@@ -1,19 +1,29 @@
-import {awsServices, PriceCacheType, InventoryItemType, inventoryApi, fetchCsPrices} from '@/src/services';
+/* eslint-disable max-depth */
 import {calculateInventoryWithPrices, getFormattedDate, minimizeInventory} from '@/server-helpers';
 import {NextApiRequest, NextApiResponse} from 'next';
-import {SteamProfileType} from '@/core/types';
-
-type InventoryRecordType = {inventory?: NoPriceInventory; update_time?: string; profile?: SteamProfileType};
-export type NoPriceInventory = Omit<InventoryItemType, 'prices'>[];
-export type inventoryCacheType = Record<string, InventoryRecordType>;
+import {
+  InventoryCacheType,
+  NoPriceInventory,
+  PriceCacheType,
+  fetchCsPrices,
+  inventoryApi,
+  awsServices
+} from '@/src/services';
 
 const THIRD_OF_THE_DAY = 8 * 60 * 60 * 1000;
 export const pricesCache: PriceCacheType = {prices: null, lastUpdated: null};
-export const inventoryCache: inventoryCacheType = {};
+export const inventoryCache: InventoryCacheType = {};
 
+// TODO: refactor and fix lint errors
 // eslint-disable-next-line max-statements, complexity
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const {steamid, isForceUpdate} = req.query as unknown as {steamid: string; isForceUpdate: boolean};
+  const {steamid, isForceUpdate} = req.query as unknown as {
+    isSteamId64: string;
+    steamid: string;
+    isForceUpdate: boolean;
+  };
+  const isSteamId64 = JSON.parse(req.query.isSteamId64 as string);
+
   if (!steamid) {
     return res.status(400).json({inventory: '[]', error: 'BAD_REQUEST'});
   }
@@ -29,39 +39,40 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (isForceUpdate) {
     try {
-      const items = await inventoryApi.get({steamid});
-      const profile = await inventoryApi.getProfile({steamid});
+      let steamId64 = steamid;
+
+      if (!isSteamId64) {
+        const {response} = await inventoryApi.getSteamIdByCustomUrl({steamid});
+        steamId64 = response.steamid;
+      }
+      const items = await inventoryApi.get({steamid: steamId64});
+      const profile = await inventoryApi.getProfile({steamid: steamId64});
+
       const minimizedInventory = minimizeInventory(items);
       const update_time = getFormattedDate(new Date());
       const withPrices = calculateInventoryWithPrices(minimizedInventory, pricesCache.prices);
 
       const {isSaved} = await awsServices.updateDynamoInventoryRecord(
-        steamid,
+        {steamid, steamId64},
+        isSteamId64,
         minimizedInventory,
         update_time,
         profile
       );
-      inventoryCache[steamid] = {inventory: minimizedInventory, update_time, profile} as inventoryCacheType;
-      return res.status(200).json({
-        profile,
-        shouldSaveSteamId: isSaved,
-        inventory: JSON.stringify(withPrices)
-      });
+      inventoryCache[steamId64 || steamid] = {inventory: minimizedInventory, update_time, profile};
+      return res.status(200).json({profile, shouldSaveSteamId: isSaved, inventory: JSON.stringify(withPrices)});
     } catch (e) {
       const error = (e as {response?: {status?: number}}) || {};
       console.error(e);
-      // eslint-disable-next-line max-depth
       if (error?.response?.status === 404) {
         return res.status(404).json({inventory: '[]'});
       }
-      // eslint-disable-next-line max-depth
       if (error?.response?.status === 403) {
         return res.status(403).json({inventory: '[]'});
       }
 
-      // eslint-disable-next-line max-depth
       if (!(steamid in inventoryCache)) {
-        const response = await awsServices.fetchFromDynamoDB(steamid, inventoryCache, pricesCache.prices);
+        const response = await awsServices.fetchFromDynamoDB(steamid, isSteamId64, inventoryCache, pricesCache.prices);
         return res.status(response.statusCode).json(response);
       }
       const {inventory, update_time, profile} = inventoryCache[steamid];
@@ -82,7 +93,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         inventory: JSON.stringify(calculateInventoryWithPrices(inventory as NoPriceInventory, pricesCache.prices))
       });
     }
-    const response = await awsServices.fetchFromDynamoDB(steamid, inventoryCache, pricesCache.prices);
+    const response = await awsServices.fetchFromDynamoDB(steamid, isSteamId64, inventoryCache, pricesCache.prices);
     return res.status(response.statusCode).json(response);
   }
 };
