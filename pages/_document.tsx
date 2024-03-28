651e7d9c1f4e04509c399130f77b0eabfaa3c1b7
Oleksandr Syvoplias <oleksandr_syvoplias@epam.com>
2024-03-28 13:14:37 +0200

    new: dev: Analytics added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, {Html, Head, Main, NextScript} from "next/document";
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+import {Analytics} from '@vercel/analytics/react';
 
 class MyDocument extends Document {
   render(): JSX.Element {
@@ -8,6 +9,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
